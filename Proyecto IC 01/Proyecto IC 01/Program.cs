3efa85fc982a06f109cace620baf2f662772630d
Luis Fernando <irigoinchamayaluisfernando@gmail.com>
2024-11-14 12:26:58 -0500

    Segunda aplicacion

--- Proyecto IC 01/Proyecto IC 01/Program.cs
+++ Proyecto IC 01/Proyecto IC 01/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Mi nombre es Luis Fernando");
+
+Console.WriteLine("Mi apellido es Chamaya irigoin ");
